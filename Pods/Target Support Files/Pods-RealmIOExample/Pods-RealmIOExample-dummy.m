664e9f4cb79119482ad337aad2ed0e96214f58e4
administrador <administrador@M72440.local>
2018-07-13 12:15:43 -0300

    Initial commit

---
+++ Pods/Target Support Files/Pods-RealmIOExample/Pods-RealmIOExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RealmIOExample : NSObject
+@end
+@implementation PodsDummy_Pods_RealmIOExample
+@end
--- RealmIOExample/ViewController.swift
+++ RealmIOExample/ViewController.swift
@@ -5,14 +5,39 @@
 //  Created by administrador on 13/07/2018.
 //  Copyright © 2018 administrador. All rights reserved.
 //
-
 import UIKit
+import Realm
+import RealmSwift
+
+class Aluno: RLMObject{
+    var matricula: String? = ""
+    var senha: String? = ""
+}
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func clicarBotao(_ sender: Any) {
+        let aluno = Aluno()
+        aluno.matricula = MatriculaTF.text
+        aluno.senha = SenhaTF.text
+        let realm = try! Realm()
+        try! realm.write{
+            realm.add(aluno)
+        }
+    }
+    
+    @IBOutlet var TelaLogin: UIView!
+   
+    @IBOutlet weak var botaoEntrar: UIButton!
+    @IBOutlet weak var SenhaTF: UITextField!
+    @IBOutlet weak var MatriculaTF: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.TelaLogin.backgroundColor = .cyan
+        self.botaoEntrar.layer.cornerRadius = self.botaoEntrar.frame.height / 2
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
